148a87fb4b4d43e6d005b58b445644705d72604a
workspace87 <prathmeshkorde89@gmail.com>
2025-07-17 00:24:59 +0530

    urls

--- backend/urls.py
+++ backend/urls.py
@@ -38,9 +38,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path("openapi/", schema_view.without_ui(cache_timeout=0), name="schema-openapi"),
     path('admin/', admin.site.urls),
     path('api/v1/', include("api.urls")),
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
