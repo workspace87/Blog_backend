d4e4a476d6a140746bfb740f52121cb7ac6a6706
workspace87 <prathmeshkorde89@gmail.com>
2025-07-23 00:35:11 +0530

    final commit

--- backend/settings.py
+++ backend/settings.py
@@ -143,8 +143,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'api.User'
 
